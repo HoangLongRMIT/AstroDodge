52f3c76aa29043a7096e8fed3e0872d852533c60
KIET HOANG <hoangthaikiet123@gmail.com>
2023-09-13 00:25:30 +0700

    git commit -m "1b FONT, TAB and HISTORY":wq

--- src/function.c
+++ src/function.c
@@ -1,8 +1,66 @@
+// ----------------------------------- function.c ------------------------------------- //
+
 #include "uart.h"
 #include "mbox.h"
 #include "framebf.h"
 #include "game_background.h"
 
+
+//====================================================================================//
+//									                  FUNCTIONS										                    //
+//====================================================================================//
+// Declare reset_arr to reset the cli buffer
+//--------------------------------------------------------------------------------
+void reset_arr(char* arr){
+	int i = 0;
+	while(arr[i] != '\0') {
+		arr[i] = '\0';
+		i++;
+	 }
+}
+// Function to reset buffer
+//--------------------------------------------------------------------------------
+void reset_ptr(const char* cli_buffer){
+	while(*cli_buffer != '\n'){
+		cli_buffer++;
+	}
+}
+// Function to compare input cli
+//--------------------------------------------------------------------------------
+int comp_str(const char* s1,const char* s2){	
+	while (*s1 != '\0' && *s2 != '\0') {
+		// Check if s1 no equal to s2
+        if (*s1 < *s2) {
+            return -1;
+        } else if (*s1 > *s2) {
+            return 1;
+        }
+        s1++;
+        s2++;
+    }
+
+    // At this point, one or both strings have ended
+    if (*s1 == '\0' && *s2 == '\0') {
+        return 0; 	// Both strings are equal
+    } else if (*s1 == '\0') {
+        return -1; 	// s1 is Null
+    } else {
+        return 1; 	// s2 is either Null or no equal s1
+    }
+}
+
+// Function return absolute int value
+//--------------------------------------------------------------------------------
+int abs(int x)
+{
+    return (x < 0) ? -x : x;
+}
+
+//====================================================================================//
+//								                IMAGE VIDEO FONT    							              	  //
+//====================================================================================//
+// Function to display the screen background image
+//--------------------------------------------------------------------------------
 void displayGameBackground(int x, int y){
   for (int h = 0; h < background_height; h++)
   {
@@ -12,6 +70,8 @@ void displayGameBackground(int x, int y){
     }
   }
 }
+// Function to clear emulator screen
+//--------------------------------------------------------------------------------
 void clearscreen(int x, int y){
   for (int h = 0; h < background_height; h++)
   {
@@ -20,4 +80,47 @@ void clearscreen(int x, int y){
       drawPixelARGB32(x + w, y + h, 0x00000000);
     }
   }
+}
+
+//====================================================================================//
+//										                DISPLAY MENU							    		  	          //
+//====================================================================================//
+// Function to display commands menu
+//--------------------------------------------------------------------------------
+void displayMenu()
+{
+    uart_puts("\n----------------------------------------------------------------------------------------------\n");
+    uart_puts(
+        "\n\tEnter a number to choose command:\n"
+        "\t1.\tDisplay text on screen\n"
+        "\t2.\tDisplay a small image\n"
+        "\t3.\tDisplay a scrollable large image\n"
+        "\t4.\tDisplay a video\n"
+        "\t5.\tPlay game\n"
+        "\t0.\tClear the screen\n");
+    uart_puts("\n----------------------------------------------------------------------------------------------\n");
+}
+
+//====================================================================================//
+//									                  WELCOME SCREEN									                //
+//====================================================================================//
+void welcome_text(){
+	uart_puts("\n----------------------------------------------------------------------------------------------\n\n");
+	uart_puts("\x1b[38;5;215md88888b d88888b d88888b d888888b .d888b.   j88D  .d888b.  .d88b.  "  );uart_puts("\n");
+				  uart_puts("88'     88'     88'     `~~88~~' VP  `8D  j8~88  88' `8D .8P  88. "  );uart_puts("\n");
+				  uart_puts("88ooooo 88ooooo 88ooooo    88       odD' j8' 88  `V8o88' 88  d'88 "  );uart_puts("\n");
+				  uart_puts("88~~~~~ 88~~~~~ 88~~~~~    88     .88'   V88888D    d8'  88 d' 88 "  );uart_puts("\n");
+				  uart_puts("88.     88.     88.        88    j88.        88    d8'   `88  d8' "  );uart_puts("\n");
+				  uart_puts("Y88888P Y88888P Y88888P    YP    888888D     VP   d8'     `Y88P'  "  );uart_puts("\n");
+				  uart_puts(" \n\n\n");
+				  uart_puts("d8888b.  .d8b.  d8888b. d88888b    .d88b.  .d8888.   ");uart_puts("\n");
+				  uart_puts("88  `8D d8' `8b 88  `8D 88'       .8P  Y8. 88'  YP   ");uart_puts("\n");
+				  uart_puts("88oooY' 88ooo88 88oobY' 88ooooo   88    88 `8bo.     ");uart_puts("\n");
+				  uart_puts("88~~~b. 88~~~88 88`8b   88~~~~~   88    88   `Y8b.   ");uart_puts("\n");
+				  uart_puts("88   8D 88   88 88 `88. 88.       `8b  d8' db   8D   ");uart_puts("\n");
+				  uart_puts("Y8888P' YP   YP 88   YD Y88888P    `Y88P'  `8888Y'   ");uart_puts("\n");
+				  uart_puts("\n\n");
+				  uart_puts("     \x1b[33mDeveloped by GROUP 12");
+				  uart_puts("\n\n\x1b[37m");
+	uart_puts("\n----------------------------------------------------------------------------------------------\n");
 }
--- src/mbox.h
+++ src/mbox.h
@@ -1,8 +1,11 @@
 // -----------------------------------mbox.h -------------------------------------
+
 #include "gpio.h"
+
 /* a properly aligned buffer */
 extern volatile unsigned int mBuf[36];
 #define ADDR(X) (unsigned int)((unsigned long)X)
+
 /* Registers */
 #define VIDEOCORE_MBOX (MMIO_BASE + 0x0000B880)
 #define MBOX0_READ (*(volatile unsigned int *)(VIDEOCORE_MBOX + 0x00))
@@ -15,12 +18,15 @@ extern volatile unsigned int mBuf[36];
 #define MBOX1_SENDER (*(volatile unsigned int *)(VIDEOCORE_MBOX + 0x34))
 #define MBOX1_STATUS (*(volatile unsigned int *)(VIDEOCORE_MBOX + 0x38))
 #define MBOX1_CONFIG (*(volatile unsigned int *)(VIDEOCORE_MBOX + 0x3C))
+
 // Request/Response code in Buffer content
 #define MBOX_RESPONSE 0x80000000
 #define MBOX_REQUEST 0
+
 // Status Value (from Status Register)
 #define MBOX_FULL 0x80000000
 #define MBOX_EMPTY 0x40000000
+
 /* channels */
 #define MBOX_CH_POWER 0 // Power management
 #define MBOX_CH_FB 1    // Frame buffer
@@ -30,12 +36,11 @@ extern volatile unsigned int mBuf[36];
 #define MBOX_CH_BTNS 5  // Buttons
 #define MBOX_CH_TOUCH 6 // Touch screen
 #define MBOX_CH_PROP 8  // Property tags (ARM -> VC)
+
 /* tags */
 #define MBOX_TAG_GETSERIAL 0x00010004 // Get board serial
 #define MBOX_TAG_GETMODEL 0x00010001  // Get board model
 #define MBOX_TAG_SETCLKRATE 0x00038002
-#define MBOX_TAG_LAST 0
-#define MBOX_TAG_SETPHYWH 0x00048003
 
 //New Tags for Screen Display
 #define MBOX_TAG_SETPHYWH 0x48003
@@ -45,6 +50,8 @@ extern volatile unsigned int mBuf[36];
 #define MBOX_TAG_SETPXLORDR 0x48006
 #define MBOX_TAG_GETFB 0x40001
 #define MBOX_TAG_GETPITCH 0x40008
+
+#define MBOX_TAG_LAST 0
 
 /* Function Prototypes */
 int mbox_call(unsigned int buffer_addr, unsigned char channel);
